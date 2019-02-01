0d63c7c20fe008f37c11a7a195bfb830ab32868a
deeptiBranch <43531285+deeptiBranch@users.noreply.github.com>
2019-02-01 17:20:01 +0530

    pre install for MPL

---
+++ preinstallstarter.java
@@ -0,0 +1,19 @@
+package com.eneff.branch.example.android;
+
+import android.app.Application;
+import io.branch.referral.Branch;
+
+public class CustomApplicationClass extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Branch logging for debugging
+        Branch.enableLogging();
+
+        // Branch object initialization
+        Branch branch = Branch.getAutoInstance(this);
+        Branch.getInstance().addInstallMetadata("channel", "oem");
+        branch.setRequestMetaData("channel", oem);
+    }
+}
